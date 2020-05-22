64f601962f2d0c61199a1e29a583b51472f61c87
Alissoul <al.gomezv@alumnos.duoc.cl>
2020-05-22 00:44:27 -0400

    Reparación Error

---
+++ src/controlador/Controlador_Credencial.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import Conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelo.Credencial;
+/**
+ *
+ * @author aliss
+ */
+public class Controlador_Credencial {
+    private Conexion Conect;
+    
+    public Controlador_Credencial() {
+        this.setConexion(Conexion.nuevaInstancia());
+    }
+    private void setConexion(Conexion nuevaConexion){
+        this.Conect = nuevaConexion;
+    } 
+    private Conexion getConexion(){
+       return this.Conect;
+    }
+    
+    public boolean agregarCredencial(Credencial nuevaCredencial){
+        boolean salida = false; 
+        try{
+            PreparedStatement pstm = this.getConexion().obtenerConexion().prepareStatement(
+                    "INSERT INTO credencial (rutAlumno) VALUES(?,?)"
+            );
+            pstm.setInt(1, nuevaCredencial.getIdCredencial());
+            if(pstm.executeUpdate()==1){
+                salida = true;
+            }
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Clase no encontrada "+e.getMessage());
+        }
+        catch(SQLException e){
+            System.out.println(String.format("Error SQL %s : %s",e.getSQLState(),e.getMessage()));
+        }
+        catch(Exception e){
+            System.out.println("otra excepion "+e.getMessage());
+        }
+        finally{
+            this.getConexion().cerrarConexion();
+        }
+        return salida;
+    }
+    
+   
+}
