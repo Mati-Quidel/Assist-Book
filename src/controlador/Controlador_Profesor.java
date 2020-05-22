c4207730e70052f895b0e5ca5553476ff472852a
Alissoul <al.gomezv@alumnos.duoc.cl>
2020-05-22 00:22:51 -0400

    Controlador Profesor

---
+++ src/controlador/Controlador_Profesor.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import Conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import modelo.Profesor;
+/**
+ *
+ * @author aliss
+ */
+public class Controlador_Profesor {
+    private Conexion Conect;
+    
+    public Controlador_Profesor() {
+        this.setConexion(Conexion.nuevaInstancia());
+    }
+    private void setConexion(Conexion nuevaConexion){
+        this.Conect = nuevaConexion;
+    } 
+    private Conexion getConexion(){
+       return this.Conect;
+    }
+    
+    public boolean agregarProfesor(Profesor nuevoProfesor){
+        boolean salida = false; 
+        try{
+            PreparedStatement pstm = this.getConexion().obtenerConexion().prepareStatement(
+                    "INSERT INTO profesor (rutProfe,dv) VALUES(?,?)"
+            );
+            pstm.setInt(1, nuevoProfesor.getRutProfe());
+            pstm.setString(2, nuevoProfesor.getDv());
+            if(pstm.executeUpdate()==1){
+                salida = true;
+            }
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("Clase no encontrada "+e.getMessage());
+        }
+        catch(SQLException e){
+            System.out.println(String.format("Error SQL %s : %s",e.getSQLState(),e.getMessage()));
+        }
+        catch(Exception e){
+            System.out.println("otra excepion "+e.getMessage());
+        }
+        finally{
+            this.getConexion().cerrarConexion();
+        }
+        return salida;
+    }
+    
+    
+}
